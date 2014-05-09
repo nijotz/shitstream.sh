2829a22ec229db3ce32a9823c5eb314dd1048980
Nick Tzaperas <nick@nijotz.com>
2014-05-09 15:42:40 -0700

    Detect audio program to use

--- client.sh
+++ client.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
+audio_programs_Darwin=(afplay)
+audio_programs_Linux=(aplay play cvlc mplayer ffplay)
+
+function get_audio_program {
+    local result=$1
+    os=$(uname)
+    #v OS is $os
+    programs_var=audio_programs_$os
+    programs=${!programs_var}
+    for _program in ${programs[@]}; do
+        whichprogram=$(which $_program)
+        extstatus=$?
+        if [ ! $exitstatus ]; then
+            #v Using program \'$whichprogram\'
+            eval $result="'$whichprogram'"
+            return
+        fi
+    done
+}
+
 while true; do
     ncat --recv-only $1 $2 > /tmp/mp3
-    afplay /tmp/mp3
+    get_audio_program program
+    $program /tmp/mp3
 done
