2264f11e5d6dd3c4112103df76f417dc43b42e7b
Nick Tzaperas <nick@nijotz.com>
2014-05-20 11:12:08 -0700

    Improved logging in mp3sender

--- mp3sender.sh
+++ mp3sender.sh
@@ -2,6 +2,10 @@
 
 source mp3base.sh
 
+# Swap stdout and stderr. Stdout will go to the terminal now and stderr will go
+# to the client
+exec 3>&2 2>&1 1>&3
+
 CURRENT="current"
 STANDBY="trumpet.mp3"
 
@@ -11,33 +15,39 @@ fi
 cd $MP3DIR
 
 # Protect against race conditions
+v "Obtaining lockfile"
 lockfile -1 -r 60 $LOCKFILE || exit 1
 trap "rm -f $LOCKFILE; exit" INT TERM EXIT
 
 # See if there's a song currently targeted for streaming
 if [ -f $CURRENT ]; then
+    v "Found a currently streaming song"
     current_mp3=$(head -n 1 $CURRENT)
     expires=$(tail -n 1 $CURRENT)
 fi
 
 # Expire the currently streaming mp3 if necessary
 if [ ! -z "$expires" ] && [ $expires -lt $(date +%s) ]; then
+    v "Expiring current song"
     rm $current_mp3
     cat "/dev/null" > $CURRENT
 fi
 
 # Stream the file if it exists
 if [ ! -z "$current" ] && [ -f "$current" ]; then
-    cat $current
+    v "Streaming current song"
+    cat $current >&2
     exit
 fi
 
 # Find the next mp3 to play
-for file in $(ls | grep '^[0-9\.]*$'); do
-    length=$(sox -t mp3 $file -n stat 2>&1 | grep Length | sed 's/[^0-9.]//g' | sed 's/\..*//')
+for file in $(ls | grep '^[0-9\.]*\.mp3$'); do
+    v "Finding another mp3 to play"
+    length=$(sox -t mp3 "$file" -n stat 2>&1 | grep Length | sed 's/[^0-9.]//g' | sed 's/\..*//')
 
     # Not a valid mp3
     if [ -z "$length" ]; then
+        v "$file is not a valid mp3, removing"
         rm $file
         continue
     fi
@@ -45,10 +55,12 @@ for file in $(ls | grep '^[0-9\.]*$'); do
     # Found a file, stream and break from loop
     echo $file > $CURRENT
     echo $(( $length + $(date +%s) ))>> $CURRENT
-    
-    cat $file
+
+    v "Streaming $file"
+    cat $file >&2
     exit
 done
 
 # Play the standby mp3 if there aren't any uploaded ones
-cat $STANDBY
+v "Playing standby"
+cat $STANDBY >&2
