6a475a55357ae6b8f54ce9b28eb6ef0539513cc7
Nick Tzaperas <nick@nijotz.com>
2014-06-04 15:04:09 -0700

    Moved player code out to it's own file

--- client/commands/play.sh
+++ client/commands/play.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 function command_play {
     helptext="Play some shit that's on the server"
     helptext="Usage: play"
@@ -14,49 +16,7 @@ function command_play {
         return 1
     fi
 
-    (
-        function update_status_bar {
-            status_connection=$1
-            status_current_mp3=$2
-            echo "status_connection=\"$1\"" > ${SHIT_DIR}/toilet
-            echo "status_current_mp3=\"$2\"" >> ${SHIT_DIR}/toilet
-            print_status_bar
-        }
-
-        function identify_mp3 {
-            id3info=$(mpg123-id3dump "$1" 2>&1)
-            artist=$(echo -e "$id3info" | sed -nr 's/Artist: (.*)/\1/p')
-            track=$(echo -e "$id3info" | sed -nr 's/Title: (.*)/\1/p')
-
-            if [ -z "$artist" ] && [ -z "$track" ]; then
-                echo "Unidentified song"
-            else
-                echo $artist - $track
-            fi
-        }
-
-        function cleanup {
-            rm -f ${SHIT_DIR}/toilet
-            for job in $(jobs -p); do
-                kill $job
-                wait $job
-            done
-        }
-        trap cleanup SIGINT SIGTERM SIGHUP EXIT
-
-        err=0
-        while [ $err -eq 0 ]; do
-            exec 4<> /dev/tcp/$shit_server/$shit_port
-            echo -e "SHIT 1\nshit_on_me\n" >&4
-            read length <&4
-            dd bs=1 count=$length <&4 > ${SHIT_DIR}/mp3 2>/dev/null
-            exec 4<&-
-            update_status_bar "Playing from $shit_server $shit_port" "$(identify_mp3 ${SHIT_DIR}/mp3)"
-            mpg123 ${SHIT_DIR}/mp3 >/dev/null 2>&1 &
-            wait $!
-            err=$?
-        done
-    ) &
+    ( source $DIR/../player.sh ) &
     stream_pid=$!
 }
 
---
+++ client/player.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function update_status_bar {
+    status_connection=$1
+    status_current_mp3=$2
+    echo "status_connection=\"$1\"" > ${SHIT_DIR}/toilet
+    echo "status_current_mp3=\"$2\"" >> ${SHIT_DIR}/toilet
+    print_status_bar
+}
+
+function identify_mp3 {
+    id3info=$(mpg123-id3dump "$1" 2>&1)
+    artist=$(echo -e "$id3info" | sed -nr 's/Artist: (.*)/\1/p')
+    track=$(echo -e "$id3info" | sed -nr 's/Title: (.*)/\1/p')
+
+    if [ -z "$artist" ] && [ -z "$track" ]; then
+        echo "Unidentified song"
+    else
+        echo $artist - $track
+    fi
+}
+
+function cleanup {
+    rm -f ${SHIT_DIR}/toilet
+    for job in $(jobs -p); do
+        kill $job
+        wait $job
+    done
+}
+trap cleanup SIGINT SIGTERM SIGHUP EXIT
+
+err=0
+while [ $err -eq 0 ]; do
+    exec 4<> /dev/tcp/$shit_server/$shit_port
+    echo -e "SHIT 1\nshit_on_me\n" >&4
+    read length <&4
+    dd bs=1 count=$length <&4 > ${SHIT_DIR}/mp3 2>/dev/null
+    exec 4<&-
+    update_status_bar "Playing from $shit_server $shit_port" "$(identify_mp3 ${SHIT_DIR}/mp3)"
+    mpg123 ${SHIT_DIR}/mp3 >/dev/null 2>&1 &
+    wait $!
+    err=$?
+done
