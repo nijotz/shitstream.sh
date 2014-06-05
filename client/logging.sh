aa4d7e3a58e5e585f84cb22a75b335d2dad9316d
Nick Tzaperas <nick@nijotz.com>
2014-06-05 13:28:19 -0700

    Client logging

---
+++ client/logging.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function startup_logging {
+    exec 2>> ${SHIT_DIR}/client.log
+    log INFO "Setup logging"
+}
+
+function log {
+    level=$(echo $1 | tr '[:lower:]' '[:upper:]')
+    shift
+    msg=$*
+
+    if [[ ! $level =~ "INFO|ERROR|DEBUG" ]]; then
+        msg="$level $msg"
+        level="ERROR"
+    fi
+
+    echo "[${level}] $msg" >&2
+}
