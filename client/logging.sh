54b4f3fa36ad42b85adb7235cacdd7c912a81f32
Nick Tzaperas <nick@nijotz.com>
2014-06-11 14:58:33 -0700

    Added configurable logging levels

--- client/logging.sh
+++ client/logging.sh
@@ -4,6 +4,12 @@
 # http://stackoverflow.com/questions/21763397/curious-tput-behavior-with-stderr-redirection
 
 SHIT_LOGGING_FORMAT=$'[$(date "+%Y-%m-%d %H:%M:%S")] [$level] $msg'
+SHIT_LOGGING_LEVEL=DEBUG
+
+_SHIT_LOGGING_ERROR=3
+_SHIT_LOGGING_WARN=2
+_SHIT_LOGGING_INFO=1
+_SHIT_LOGGING_DEBUG=0
 
 function startup_logging {
     exec 9>> ${SHIT_DIR}/client.log
@@ -13,15 +19,24 @@ function cleanup_logging {
     exec 9>&-
 }
 
+function logging_number {
+    eval echo \$_SHIT_LOGGING_$1
+}
+
 function log {
     level=$(echo $1 | tr '[:lower:]' '[:upper:]')
     shift
     msg=$*
 
-    if [[ ! $level =~ INFO|ERROR|DEBUG ]]; then
+    level_num=$(logging_number $level)
+    min_num=$(logging_number $SHIT_LOGGING_LEVEL)
+    if [[ -z $level_num ]]; then
         msg="$level $msg"
         level="ERROR"
+        level_num=$(logging_number $level)
     fi
 
-    eval echo $SHIT_LOGGING_FORMAT >&9
+    if [[ $level_num -ge min_num ]]; then
+        eval echo $SHIT_LOGGING_FORMAT >&9
+    fi
 }
